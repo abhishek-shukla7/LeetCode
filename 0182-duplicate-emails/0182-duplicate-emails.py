8c57f776504d628e3517187566f8abdb2443280c
Abhishek Shukla <abhisame077@gmail.com>
2026-01-11 19:16:48 +0530

    Time: 293 ms (27.81%), Space: 67.1 MB (75.48%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+  return (
+        person[person.duplicated(subset="email", keep=False)]
+        [["email"]]
+        .drop_duplicates()
+        .rename(columns={"email": "Email"})
+    )
