379199cde9c27a5701ce7c46b1cfab534b3659a5
Abhishek Shukla <abhisame077@gmail.com>
2026-01-14 14:01:49 +0530

    Time: 0 ms (100%), Space: 13.5 MB (12.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,15 +5,4 @@ class Solution(object):
         :rtype: int
         """
         n=len(nums)
-        dict={}
-
-        for i in range(0,n+1):
-            dict[i]=0
-
-        for nu in nums:
-            dict[nu]=1
-
-        for k,u in dict.items():
-            if u==0:
-                return k
-        
+        return n*(n+1)//2-sum(nums)
